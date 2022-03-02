13579f85d50fdd76a805c080eacc475cd8db7db9
wagnerj5 <wagnerj5@tcnj.edu>
2022-03-02 00:17:29 -0500

    update bottom margin for keyboard

--- src/components/keyboard/Keyboard.tsx
+++ src/components/keyboard/Keyboard.tsx
@@ -52,7 +52,7 @@ export const Keyboard = ({
   }, [onEnter, onDelete, onChar])
 
   return (
-    <div>
+    <div className="pb-8">
       <div className="flex justify-center mb-1">
         {['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'].map((key) => (
           <Key
