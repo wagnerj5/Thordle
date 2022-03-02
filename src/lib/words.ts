4e9074b97fb939232c383c09fe236e9f7d01b6c8
wagnerj5 <wagnerj5@tcnj.edu>
2022-03-01 23:33:12 -0500

    updated epoch time

--- src/lib/words.ts
+++ src/lib/words.ts
@@ -74,7 +74,7 @@ export const localeAwareUpperCase = (text: string) => {
 
 export const getWordOfDay = () => {
   // January 1, 2022 Game Epoch
-  const epochMs = new Date('February 28, 2022 00:00:00').valueOf()
+  const epochMs = new Date('March 1, 2022 00:00:00').valueOf()
   const now = Date.now()
   const msInDay = 86400000
   const index = Math.floor((now - epochMs) / msInDay)
